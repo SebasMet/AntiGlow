2d169b8b86d08c1f1d7b1b552d3385511d8d6cbc
SebasMet <sebastian.metzger.nl@gmail.com>
2025-01-19 14:00:41 +0100

    Added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { LandingPageComponent } from './core/containers/landing-page/landing-page.component';
 import { QuizPageComponent } from './quiz/containers/quiz-page/quiz-page.component';
 import { CourseViewComponent } from './course-overview/containers/course-view/course-view.component';
 import { LoginPageComponent } from './core/containers/login-page/login-page.component';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']); 
+
+
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'course/:id', component: QuizPageComponent },
-  { path: 'overview', component: CourseViewComponent}
+  { path: 'course/:id', component: QuizPageComponent, canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+  { path: 'overview', component: CourseViewComponent, canActivate: [AuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } }
 ];
 
 @NgModule({
